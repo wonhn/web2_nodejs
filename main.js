2a259372016da41ec5b67900d085da99451abe66
nana <61894088+wonhn@users.noreply.github.com>
2021-01-03 12:03:41 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+      return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+    // response.end(__dirname + url);
+    // response.end('hhhh : ' + url);
+
+});
+app.listen(3000);
